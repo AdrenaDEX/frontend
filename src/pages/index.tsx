03724079e60065d48be72f53e4ad1634d0491676
Orex <adrena.orex@gmail.com>
2024-09-18 08:29:58 +0400

    nit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,10 +4,5 @@ import { useEffect } from 'react';
 export default function Home() {
   const router = useRouter();
 
-  // Unused page, redirect to /genesis for now
-  // useEffect(() => {
-  //   router.push('/genesis');
-  // }, [window.location.pathname]);
-
   return <main className="w-full h-full flex" />;
 }
